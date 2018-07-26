11bef284aa5396649b58d9ffa9800123d4301e6c
Alan Ayoub <alanayoub@me.com>
2018-07-25 20:15:08 -0700

    added citation and year, working out the printQuote function

--- js/script.js
+++ js/script.js
@@ -13,72 +13,65 @@ function randomQuoteGenerator() {
 var quotes = [
   {
     quote:  "Everything you've ever wanted is on the other side of fear.",
-    source: "Geroge Addair",
-    citation: "",
-    year: ""
+    source: "Geroge Addair"
+  },
+  {
+    quote:  "The mind is everything. What you think you become.",
+    source: "Buddha"
+  },
+  {
+    quote:  "Believe you can and you're halfway there.",
+    source: "Theodore Roosevelt"
+  },
+  {
+    quote:  "A person who never made a mistake never tried anything new.",
+    source: "Albert Einstein"
   },
   {
     quote:  "Perhaps the most important principle for the good algorithm designer is to refuse to be content.",
     source: "Alfred V. Aho",
-    citation: "",
-    year: ""
+    citation: "The Design and Analysis of Computer Algorithms",
+    year: "1974"
   },
   {
     quote:  "Whatever the mind of man can conceive and believe, it can achieve.",
     source: "Napoleon Hill",
-    citation: "",
-    year: ""
+    citation: "Think and Grow Rich",
+    year: "1937"
   },
   {
     quote:  "Strive not to be a success, but rather to be of value.",
     source: "Albert Einstein",
-    citation: "",
-    year: ""
+    citation: "LIFE Magazine",
+    year: "1952"
   },
   {
     quote:  "You miss 100% of the shots you don't take.",
     source: "Wayne Gretsky",
-    citation: "",
-    year: ""
-  },
-  {
-    quote:  "The mind is everything. What you think you become.",
-    source: "Buddha",
-    citation: "",
-    year: ""
-  },
-  {
-    quote:  "Believe you can and you're halfway there.",
-    source: "Theodore Roosevelt",
-    citation: "",
-    year: ""
-  },
-  {
-    quote:  "A person who never made a mistake never tried anything new.",
-    source: "Albert Einstein",
-    citation: "",
-    year: ""
-  },
+    citation: "edition of the Hockey News",
+    year: "1983"
+  }
 
 ];
 
-
-// testing my array
-console.log(quotes[0]);
-console.log(quotes[5]);
-console.log(quotes[7]);
-
-
 // Create the getRandomQuuote function and name it getRandomQuote
 function getRandomQuote(array) {
-
+  var Num = randomQuoteGenerator();
+  var getRandomQuote = quotes[Num];
+  return getRandomQuote;
 }
-
 
 // Create the printQuote funtion and name it printQuote
 function printQuote() {
+  // Calls random quote and stores it in a variable
+  var quote = getRandomQuote();
+  // Constructs a string containing different properties using html
+  console.log(quote);
+  // if/else to handle missing citation or year
 
 }
+
+printQuote();
 
 
 // This event listener will respond to "Show another quote" button clicks
